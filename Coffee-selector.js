9a62d0a24c58a685dba5e5d4e27828014dd8e3fd
Sarah Brady <sarahbrady84@gmail.com>
2015-01-26 09:09:25 -0800

    It sorts (yay) but it does it too fast.

--- Coffee-selector.js
+++ Coffee-selector.js
@@ -1,39 +1,24 @@
-$lis = $("li");
-
-function sort(cup) {
-  $lis.eq(i).data(cup)
-}
-function sort(atmos) {
-  $lis.eq(i).data(atmos)
-}
-function sort(local) {
-  $lis.eq(i).data(local)
-}
-
-var $lis = $("li");
 
 function insertionSort(lis) {
-    var len = lis.length,
-        value,
-        i,
-        j;
-    for (i=0; i < len; i++) {
-        value = lis[i];
-        for (j=i-1; j > -1 && lis[j] > value; j--) {
-            lis[j+1] = lis[j];
-        }
+// setTimeout(function() {
+//    } 2000);
 
-        lis[j+1] = value;
-    }
+var timeMultiplier = 0;
+var $lis = $("li");
+for(var i = 0; i < $lis.length; i++) {
+  var $current = $lis.eq(i);
 
-    return lis;
+  while ($current.data("cup") < $current.prev() .data("cup")) {
+    $current.insertBefore($current.prev());
+  }
+}
 }
 $("button").on("click", function() {
   var beep = [5, 1, 9, 1, 4, 8];
   insertionSort(beep);
   console.log(beep);
 });
-
 // $( "li" ).each(function( index ) {
 //   console.log( index + " " + $( this ).text() );
 // });
+
